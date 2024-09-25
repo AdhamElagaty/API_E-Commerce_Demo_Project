75e82e83c73a59b93f1d1a5006535d8baf77cad5
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-25 11:09:10 +0300

    Product Service

---
+++ E-Commerce.Service/Services/ProductService/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Service.Services.ProductService.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Service.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<ProductDetailsDto> GetProductByIdAsync(int? productId);
+        Task<IReadOnlyList<ProductDetailsDto>> GetAllProductsAsync();
+        Task<IReadOnlyList<BrandTypeDetailsDto>> GetAllBrandsAsync();
+        Task<IReadOnlyList<BrandTypeDetailsDto>> GetAllTypesAsync();
+    }
+}
