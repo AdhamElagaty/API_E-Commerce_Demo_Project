ca58e1f3d320ec96039a24523b68a898196c27a7
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-28 05:34:44 +0300

    Product Specifications

---
+++ E-Commerce.Repository/Specification/ProductSpecs/ProducSpecification.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Data.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Specification.ProductSpecs
+{
+    public class ProductWithSpecifications : BaseSpecification<Product>
+    {
+        public ProductWithSpecifications(ProductSpecification specs)
+            : base(product => (!specs.BrandId.HasValue || product.BrandId == specs.BrandId.Value) &&
+                                    (!specs.TypeId.HasValue || product.TypeId == specs.TypeId.Value)
+                )
+        {
+            AddInclude(x => x.Brand);
+            AddInclude(x => x.Type);
+
+        }
+    }
+}
