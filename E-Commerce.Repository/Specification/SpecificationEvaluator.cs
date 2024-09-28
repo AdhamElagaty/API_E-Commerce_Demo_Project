6065e45b932b56752c9db65eb2a09f21949163a2
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-28 04:38:45 +0300

    Prepare Specification Classes

---
+++ E-Commerce.Repository/Specification/SpecificationEvaluator.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Specification
+{
+    public class SpecificationEvaluator<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+       
+    }
+}
