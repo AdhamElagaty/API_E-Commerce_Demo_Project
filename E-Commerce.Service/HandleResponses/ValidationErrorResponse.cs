e7beb597a47b782371e24f602f70e2e76d451d73
Adham Elagaty <adhamalagaty@gmail.com>
2024-10-03 10:40:53 +0300

    Exception MiddleWare (Needed Classes)

---
+++ E-Commerce.Service/HandleResponses/ValidationErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Service.HandleResponses
+{
+    public class ValidationErrorResponse : CustomException
+    {
+        public ValidationErrorResponse() : base(400)
+        {
+        }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
