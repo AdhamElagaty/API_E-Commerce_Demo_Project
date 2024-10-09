ca0cda0a320e12f309caf530811cd9a2803934c8
Adham Elagaty <adhamalagaty@gmail.com>
2024-10-09 23:36:43 +0300

    TokenService

---
+++ E-Commerce.Service/Services/TokenService/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce.Data.Entities.IdentityEntities;
+
+namespace E_Commerce.Service.Services.TokenService
+{
+    public interface ITokenService
+    {
+        string GenerateToken(AppUser user);
+    }
+}
