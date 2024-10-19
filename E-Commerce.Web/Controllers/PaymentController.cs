89f74aeb594bf5326b9f28ea3fd5226b082ec40a
Adham Elagaty <adhamalagaty@gmail.com>
2024-10-19 19:49:30 +0300

    PaymentController & Install Stripe

---
+++ E-Commerce.Web/Controllers/PaymentController.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Service.Services.BasketService.Dtos;
+using E_Commerce.Service.Services.PaymentService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace E_Commerce.Web.Controllers
+{
+    public class PaymentController : BaseController
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasketDto>> CreateOrUpdatePaymentIntent(CustomerBasketDto input)
+            => Ok(await _paymentService.CreateOrUpdatePaymentIntent(input));
+    }
+}
